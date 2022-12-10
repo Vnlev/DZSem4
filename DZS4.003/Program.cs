09dd26b5552ae8f2f37ba503da86a1ca116147b5
Vladislav <vnl.i@ya.ru>
2022-12-10 16:02:37 +0300

    Домашняя задача 3 семинар 4

---
+++ DZS4.003/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный случайными числами. Оформите заполнение массива и вывод 
+// в виде функции (пригодится в следующих задачах). 
+// Реализовать через функции. 
+// (* Доп сложность, 
+// “введите количество элементов массива”, 
+// “Введите минимальный порог случайных значений”, 
+// “Введите максимальный порог случайных значений”)
+
+int Promt(string messege)      //Функция считывает число
+{
+    System.Console.Write($"{messege}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int length, int minrange, int maxrange)    //Объявление функции ; не надо ставить 
+{
+    int[] array = new int [length];
+    for (int i = 0; i < array.Length;i++)        //Цикл пока
+    {
+       array[i] = new Random().Next(minrange, maxrange + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+   for (int j = 0; j < array.Length; j++)
+   {
+        System.Console.Write($"{array[j]} ");
+
+   }
+}
+int length = Promt("Введите длину массива");
+int minrange = Promt("Введите минимальную цифру в образовании массива");
+int maxrange = Promt("введите максимальную цифру в образовании массива");
+
+int[] array = GenerateArray(length, minrange, maxrange); 
+PrintArray(array);
